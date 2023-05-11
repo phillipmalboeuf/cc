24f245437052bdc0f856c6eab0665bf031bc8e52
Phillip Malboeuf <phil@phils.computer>
2023-05-11 08:31:05 -0400

    assets

--- next.config.js
+++ next.config.js
@@ -11,6 +11,12 @@ const nextConfig = {
         port: '',
         pathname: '/5pywcqx1gkry/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'assets.ctfassets.net',
+        port: '',
+        pathname: '/5pywcqx1gkry/**',
+      },
     ],
   },
 }
